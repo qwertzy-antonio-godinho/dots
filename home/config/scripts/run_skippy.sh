f26cbc0f2a5ee5cbf20ffc273ca12514ec5edffe
qwertzy-antonio-godinho <qwertzy-antonio-godinho@gmail.com>
2022-09-24 16:19:59 +0100

    update skippy-xd

--- home/config/scripts/run_skippy.sh
+++ home/config/scripts/run_skippy.sh
@@ -1,19 +1,13 @@
-#! /usr/bin/bash
+#!/bin/bash
 
-msgId="34782933"
+IS_RUNNING="$(pgrep skippy-xd)"
 
-# check if process is running ( 0 = off, 1 = on )
-STATUS="$(ps -ef | grep -w '[s]kippy' | wc -l)"
-# if off then turn on
-if [[ "${STATUS}" == 0 ]]; then
-  [[ -z "$1" ]]
-  skippy-xd --config ~/.config/skyppy-xd/skippy-xd.rc --start-daemon /dev/null 2>&1 & disown 
+if [ "${IS_RUNNING}" -eq 0 ]; then
+	skippy-xd --config ~/.config/skyppy-xd/skippy-xd.rc --start-daemon /dev/null 2>&1 & disown 
 fi
 
-# check if there are any windows available to show ( 0 = off, 1 = on )
-STATUS="$(wmctrl -l | grep -v "conky" | grep icarus | wc -l)"
-# if off then turn on
-if [[ "${STATUS}" > 0 ]]; then
-  [[ -z "$1" ]]
-  skippy-xd --toggle-window-picker
+HAS_WINDOWS="$(wmctrl -l | grep -v -E "conky|stalonetray" | grep -c "$(hostname)")"
+
+if [ "${HAS_WINDOWS}" -gt 0 ]; then
+	skippy-xd --toggle-window-picker
 fi
