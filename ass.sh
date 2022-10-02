41d401246e4fbfade9479643fd985acf7889d0b6
qwertzy-antonio-godinho <qwertzy-antonio-godinho@gmail.com>
2022-10-02 11:50:16 +0100

    only base packages without a window manager

--- ass.sh
+++ ass.sh
@@ -1,9 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
 PROJECT_ROOT="${PWD}"
 SCRIPT_NAME=$(basename "$0")
 LTS_SUPPORT="" #-lts
 USER_NAME=$(whoami)
+YAY="https://aur.archlinux.org/yay"
 
 declare -a PACKAGES=(
 	"archlinux-keyring"
@@ -22,7 +23,6 @@ declare -a PACKAGES=(
 	"eom"
 	"ffmpegthumbnailer"
 	"fzf"
-	"fluxbox"
 	"festival"
 	"festival-us"
 	"git"
@@ -34,8 +34,8 @@ declare -a PACKAGES=(
 	"htop"
 	"hsetroot"
 	"jq"
-	"linux$LTS_SUPPORT"
-	"linux$LTS_SUPPORT-headers"
+	"linux${LTS_SUPPORT}"
+	"linux${LTS_SUPPORT}-headers"
 	"lib32-gnutls"
 	"lib32-vulkan-icd-loader"
 	"lib32-vkd3d"
@@ -57,7 +57,7 @@ declare -a PACKAGES=(
 	"nmap"
 	"network-manager-applet"
 	"ntfs-3g"
-	"nvidia$LTS_SUPPORT"
+	"nvidia${LTS_SUPPORT}"
 	"nvidia-settings"
 	"nvidia-utils"
 	"openal"
@@ -65,9 +65,7 @@ declare -a PACKAGES=(
 	"openssl"
 	"opusfile"
 	"p7zip"
-#	"pass"
 	"pavucontrol"
-#	"picom"
 	"pluma"
 	"projectm"
 	"projectm-pulseaudio"
@@ -79,7 +77,6 @@ declare -a PACKAGES=(
 	"qt5ct"
 	"qtcurve-qt5"
 	"redshift"
-#	"ripgrep"
 	"rofi"
 	"solaar"
 	"steam"
@@ -89,15 +86,12 @@ declare -a PACKAGES=(
 	"thunderbird"
 	"unrar"
 	"unzip"
-	"unclutter"
 	"virt-manager"
 	"vulkan-icd-loader"
 	"vulkan-tools"
 	"vkd3d"
 	"vnstat"
 	"volumeicon"
-#	"wmctrl"
-	"xbindkeys"
 	"xclip"
 	"xcursor-vanilla-dmz-aa"
 	"xorg-server"
@@ -115,49 +109,46 @@ declare -a ABS=(
     "aic94xx-firmware"
     "brave-bin"
 	"bluetooth-autoconnect"
-#	"pass-coffin"
-	"fittstool"
-	"skippy-xd-git"
 	"visual-studio-code-bin"
 	"xf86-input-mtrack-git"
     "wd719x-firmware"
 )
 
-function check_previleges () {
-    printf "\nValidating run previleges... user $USER_NAME ("$EUID")\n"
-    if [ "$EUID" -eq 0 ]
+check_previleges () {
+    printf "\nValidating run previleges... user ${USER_NAME} (${EUID})\n"
+    if [ "${EUID}" -eq 0 ]
       then printf "Please run as a regular user to continue...\n\n"
       exit
     fi
 }
 
-function update_system () {
+update_system () {
     printf "\nUpdating system...\n"
     sudo pacman -Suyq --color=always --noconfirm
 }
 
-function process_pacman () {
+process_pacman () {
     printf "\nInstalling Pacman packages...\n"
     for PACKAGE in "${PACKAGES[@]}"; do
-        printf "$PACKAGE\n"
-        sudo pacman -S --color=always --noconfirm "$PACKAGE"
+        printf "${PACKAGE}\n"
+        sudo pacman -S --color=always --noconfirm "${PACKAGE}"
     done
 }
 
-function install_yay () {
+install_yay () {
     printf "\nInstalling yay...\n"
     cd "${PROJECT_ROOT}"
-    git clone https://aur.archlinux.org/yay 
+    git clone "${YAY}" 
     cd yay
     makepkg -sirc --noconfirm
     cd "${PROJECT_ROOT}"
 }
 
-function process_abs () {
+process_abs () {
     printf "\nInstalling ABS (yay) packages...\n"
     for PACKAGE in "${ABS[@]}"; do
-        printf "$PACKAGE\n"
-        yay -S --noconfirm "$PACKAGE"
+        printf "${PACKAGE}\n"
+        yay -S --noconfirm "${PACKAGE}"
     done
 }
 
@@ -165,7 +156,7 @@ function process_abs () {
 #
 #
 
-function configure_system () {
+configure_system () {
     printf "\nConfiguring system...\n"
     sudo cp -r -v "${PROJECT_ROOT}"/etc/* /etc/
     sudo cp -r -v "${PROJECT_ROOT}"/usr/* /usr/
@@ -176,53 +167,77 @@ function configure_system () {
     sudo systemctl enable ufw
     sudo systemctl enable libvirtd
     sudo systemctl enable NetworkManager
-    sudo vnstat --add -i $(ip -o link show | awk '{print $2,$9}' | grep UP | awk '{print $1}' | sed 's/://')
+    sudo vnstat --add -i "$(ip -o link show | awk '{print $2,$9}' | grep UP | awk '{print $1}' | sed 's/://')"
     sudo systemctl enable vnstat
     sudo systemctl restart vnstat
-	sudo setcap cap_net_raw,cap_net_admin,cap_net_bind_service+eip /usr/bin
-    sudo mkinitcpio -p linux$LTS_SUPPORT
+	sudo setcap cap_net_raw,cap_net_admin,cap_net_bind_service+eip "$(which nmap)"
+    sudo mkinitcpio -p "linux${LTS_SUPPORT}"
 }
 
-function configure_user () {
+configure_user () {
     printf "\nConfiguring user accounts...\n"
     systemctl --user enable pulseaudio
     systemctl --user start pulseaudio
     pactl set-sink-mute @DEFAULT_SINK@ toggle
     pactl -- set-sink-volume @DEFAULT_SINK@ 80%
-    eval $(ssh-agent)
-	#systemctl --user enable gpg-agent.socket
-	systemctl --user enable dirmngr.socket gpg-agent.socket gpg-agent-ssh.socket gpg-agent-browser.socket gpg-agent-extra.socket
-	mkdir -p ~/.gnupg && chmod 700 ~/.gnupg
+    eval "$(ssh-agent)"
+#	systemctl --user enable gpg-agent.socket
+#	systemctl --user enable dirmngr.socket gpg-agent.socket gpg-agent-ssh.socket gpg-agent-browser.socket gpg-agent-extra.socket
+#	mkdir -p ~/.gnupg && chmod 700 ~/.gnupg
 #	gpg --import /backup/.keys/privkey.asc
 #	gpg --import /backup/.keys/public.key
 	mkdir -p ~/.ssh && touch ~/.ssh/known_hosts && ssh-keyscan -t Ed25519 github.com > ~/.ssh/known_hosts
     ssh-add /backup/.keys/qwertzy-antonio-godinho-github.com
-	sudo gpasswd -a $USER_NAME input
-	sudo gpasswd -a $USER_NAME libvirt
+	sudo gpasswd -a "${USER_NAME}" input
+	sudo gpasswd -a "${USER_NAME}" libvirt
 }
 
-function cleanup () {
+cleanup () {
     printf "\nCleaning up the system...\n"
     sudo pacman -Rus --color=always --noconfirm vim
     rm -rf "${PROJECT_ROOT}"/yay
     yay -Yc --noconfirm
 }
 
-printf "\n${SCRIPT_NAME} - Automated System Setup\n"
-check_previleges
-update_system
-process_pacman
-install_yay
-process_abs
-configure_system
-configure_user
-cleanup
-printf "\n /// FINISHED ${SCRIPT_NAME} ///\n\n"
-while true; do
-    read -p "Do you wish to reboot the system? " yn
-    case $yn in
-        [Yy]* ) reboot; break;;
-        [Nn]* ) exit;;
-        * ) echo "Please type [Y/y] yes or [N/n] no.";;
-    esac
-done
+main () {
+	printf "\n${SCRIPT_NAME} - Automated System Setup\n"
+	printf "\n /// IMPORTANT: This script should be used after a fresh archlinux installation.///\n\n"
+	while true; do
+		read -r -p "Do you wish to continue with the installation? " yn
+		case $yn in
+			[Yy]*) 
+				check_previleges
+				update_system
+				process_pacman
+				install_yay
+				process_abs
+				configure_system
+				configure_user
+				cleanup
+				printf "\n /// FINISHED ${SCRIPT_NAME} ///\n\n"
+				while true; do
+					read -r -p "Do you wish to reboot the system? " yn
+					case $yn in
+						[Yy]*) 
+							reboot
+						;;
+						[Nn]*) 
+							exit 0
+						;;
+						*) 
+							echo "Please use [Y/y] for 'Yes' or [N/n] for 'No'."
+						;;
+					esac
+				done
+			;;
+			[Nn]*) 
+				exit 0
+			;;
+			*) 
+				echo "Please use [Y/y] for 'Yes' or [N/n] for 'No'."
+			;;
+		esac
+	done
+}
+
+main
