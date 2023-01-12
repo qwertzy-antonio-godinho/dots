0d02d625e6fe6601c0fddb2a04f1f590cf0479c9
qwertzy-antonio-godinho <qwertzy-antonio-godinho@gmail.com>
2023-01-12 23:03:12 +0000

    Add git-log-summary vis

--- home/config/bash/alias.sh
+++ home/config/bash/alias.sh
@@ -18,6 +18,7 @@ alias most="most -w"
 alias git-status="git status -sb --show-stash --ahead-behind"
 alias git-commit-dry="git commit --dry-run --short --branch"
 alias git-log="git log --graph --pretty=format:'%Cred%h%Creset %ad %s%C(yellow) %d%Creset %C(bold blue)<%an>%Creset' --date=short"
+alias git-log-summary="git log --stat --graph --color"
 
 # TV
 alias tv-small="nvidia-settings --assign CurrentMetaMode='HDMI-0: 1920x1080_60 +0+0 {viewportout=1840x1035+40+22} {ForceFullCompositionPipeline=On}'"
